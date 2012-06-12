8bf2a8888a00e3900f084e0b5fe72e174074568a
Ely Apao <jozzua@gmail.com>
2012-06-12 18:28:04 +0800

    routes test

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ LonLat::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'resources#new'
+   root :to => 'resources#index'
 
   # See how all your routes lay out with "rake routes"
 
